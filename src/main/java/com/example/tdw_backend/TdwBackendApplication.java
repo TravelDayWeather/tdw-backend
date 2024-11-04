a69efc75920fe085846541d4d3d1b6005a8539a1
jangseonho <sunho3456@naver.com>
2024-11-04 17:28:55 +0900

    test

---
+++ src/main/java/com/example/tdw_backend/TdwBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.tdw_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdwBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TdwBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tdw_backend/TdwBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tdw_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TdwBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
